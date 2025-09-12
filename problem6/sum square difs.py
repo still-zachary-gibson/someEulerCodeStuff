cabe90c9e5402d227ba84f919664630d41d56ed2
still-zachary-gibson <zacharygibson487@gmail.com>
2025-09-12 12:41:23 -0400

    Create sum square difs.py

---
+++ problem6/sum square difs.py
@@ -0,0 +1,13 @@
+oh_god = 0
+
+for i in range(101):
+    oh_god += i*i
+
+oh_goodness = 0
+
+for i in range(101):
+    oh_goodness += i
+
+oh_goodness = oh_goodness * oh_goodness
+
+print(oh_goodness - oh_god)

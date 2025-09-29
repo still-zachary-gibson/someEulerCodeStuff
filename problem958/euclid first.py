857640660cec8bf8ef7995f109caf4f3f8a80215
still-zachary-gibson <zacharygibson487@gmail.com>
2025-09-29 13:05:36 -0400

    not fully sure what im doing
    it got very stuck with small integers so im gonna try to fix that

--- problem958/euclid first.py
+++ problem958/euclid first.py
@@ -1,17 +1,24 @@
 def euclid_algo(n,m):
     steps = 0
-    while n != m:
+    while m != 0:
         if n > m:
             n -= m
         else:
             m -= n
+        #if n < m:
+            #n,m = m,n
+        #n -= m
         steps += 1
+        '''if n%m == 0:
+            steps += n/m
+            n =
+            ''' 
     return n, steps
 
 smallest_numb = (0,999999999999)
-numb = 10**12 + 39
+numb = 89 # 10**12 + 39
 
-for i in range(1,numb):
+for i in range(1+3,numb):
     results = euclid_algo(numb,i)
     if results[0] != 1:
         continue

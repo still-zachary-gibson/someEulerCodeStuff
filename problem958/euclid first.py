aded0814c78d0192f7f58a49e079cb1c41eb31f5
still-zachary-gibson <zacharygibson487@gmail.com>
2025-09-29 14:34:44 -0400

    best i culd do

--- problem958/euclid first.py
+++ problem958/euclid first.py
@@ -1,22 +1,33 @@
 def euclid_algo(n,m):
     steps = 0
-    while m != 0 and n != 0:
+    times_since_changed = [0,0]
+    while m != n:
         if n > m:
-            n= n % m
+            times_since_changed[0] = -1
+            n -= m
         else:
-            m = m % n
-        #if n < m:
-            #n,m = m,n
-        #n -= m
+            times_since_changed[1] = -1
+            m -= n
+        '''
+        times_since_changed[0] += 1
+        times_since_changed[1] += 1
+        if times_since_changed[1] > 20:
+            steps += n/m
+            n = m
+        elif times_since_changed[0] > 20:
+            steps += m/n
+            m = n
+        else:
+        '''
         steps += 1
     return n, steps
 
 smallest_numb = (0,999999999999)
-numb = 89 # 10**12 + 39
+numb = 10**12 + 39
 
-print(euclid_algo(89, 34))
+#print(euclid_algo(89, 34))
 
-for i in range(1+3,numb):
+for i in range(1,numb):
     results = euclid_algo(numb,i)
     if results[0] != 1:
         continue

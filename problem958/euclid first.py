8167b200dd392f14e45a7b1c15da961d3ad6090a
still-zachary-gibson <zacharygibson487@gmail.com>
2025-09-29 13:46:08 -0400

    oh damn it the better method changes the answer. oh good god

--- problem958/euclid first.py
+++ problem958/euclid first.py
@@ -1,22 +1,20 @@
 def euclid_algo(n,m):
     steps = 0
-    while m != 0:
+    while m != 0 and n != 0:
         if n > m:
-            n -= m
+            n= n % m
         else:
-            m -= n
+            m = m % n
         #if n < m:
             #n,m = m,n
         #n -= m
         steps += 1
-        '''if n%m == 0:
-            steps += n/m
-            n =
-            ''' 
     return n, steps
 
 smallest_numb = (0,999999999999)
 numb = 89 # 10**12 + 39
+
+print(euclid_algo(89, 34))
 
 for i in range(1+3,numb):
     results = euclid_algo(numb,i)

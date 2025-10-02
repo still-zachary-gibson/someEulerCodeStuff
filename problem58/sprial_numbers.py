ff314ef34a14bb2130ed378228a8199dd7caee2b
still-zachary-gibson <zacharygibson487@gmail.com>
2025-10-02 14:25:02 -0400

    this should work, if you're seeing this message it did

--- problem58/sprial_numbers.py
+++ problem58/sprial_numbers.py
@@ -20,44 +20,40 @@ def print_spiral(spi): #brain hurts
             else:
                 temp.append(str(j))
         print(temp)
+#Test function so like not used for realsies
 
 #I need to optimize, and reuse for some time gain.
 def generate_sprial(size,clockwise=True):
     global the_spiral
-    do_we_have = True
     try:
-        test = (the_spiral)
-    except:
-        do_we_have = False
-    cur_dir = 0 #0 right, 1 down, 2 left, 3 up
-    if do_we_have: #new code for awesomeness
-        #it only works for +2 size increases but i don't care enough currently
-        #OH THAT'S SUCH A SPEED INCREASE
         cool_spiral = the_spiral
         for i in cool_spiral:
             i.insert(0,0)
             i.append(0)
         cool_spiral = [[0]*size] + cool_spiral + [[0]*size]
         if clockwise:
-            number_to_yeah = cool_spiral[1][size-2]
-            current_postion = [1, size-2]
+            number_to_yeah = cool_spiral[1][size-2]+2
+            cool_spiral[1][size-1] = number_to_yeah-1
+            cur_dir = 1
+            current_postion = [2, size-1]
+            allowed_length = size-2
         else:
-            number_to_yeah = cool_spiral[size-2][size-2]
-            current_postion = [size-2, size-2]
-        #spooky_numb = 1
-    else:
+            number_to_yeah = cool_spiral[size-2][size-2]+2
+            current_postion = [size-3, size-1]
+            cur_dir = 3
+            cool_spiral[size-2][size-1] = number_to_yeah-1
+            allowed_length = size-2
+    except:
         cool_spiral = []
         for i in range(size):
             cool_spiral.append([0]*size)
         number_to_yeah = 1
         current_postion = [math.floor(size/2), math.floor(size/2)] #y, x
         cool_spiral[current_postion[0]][current_postion[1]] = number_to_yeah
-        number_to_yeah += 1
+        number_to_yeah = 2
         current_postion[(cur_dir+1)%2] += 1*(1-(math.floor(cur_dir/2)*2))
-        #spooky_numb = math.floor(size/2)
-    allowed_length = 1
+        allowed_length = 1
     while number_to_yeah <= size**2:
-        #cool_spiral[current_postion[0]][current_postion[1]] = number_to_yeah
         if cur_dir == 0:
             cool_spiral[current_postion[0]][current_postion[1]:current_postion[1]+allowed_length] = list(range(number_to_yeah, number_to_yeah+allowed_length))
             current_postion[(cur_dir+1)%2] += allowed_length-1
@@ -76,17 +72,11 @@ def generate_sprial(size,clockwise=True):
                 cool_spiral[i][current_postion[1]] = number_to_yeah
                 number_to_yeah += 1
             current_postion[0] -= allowed_length-1
-        print_spiral(cool_spiral)
-        print(current_postion)
-        #look_for = size if cur_dir < 2 else -1 #i didn't notice this was different,,,
-        #if current_postion[(cur_dir+1)%2] + spooky_numb*(1-(math.floor(cur_dir/2)*2)) == look_for:
         if cur_dir % 2 == 1:
             if allowed_length+1 < size:
                 allowed_length += 1
         cur_dir = (cur_dir+1*((clockwise*2)-1)) % 4
         current_postion[(cur_dir+1)%2] += 1*(1-(math.floor(cur_dir/2)*2))
-        #else:
-        #    current_postion[(cur_dir+1)%2] += 1*(1-(math.floor(cur_dir/2)*2))
     return cool_spiral
 
 def calc_corners(spiral):
@@ -103,12 +93,7 @@ def calc_corners(spiral):
 def calc_prime_corners(spiral):
     global prime_amount
     size = len(spiral)-1
-    exists = True
     try:
-        test = prime_amount
-    except:
-        exists = False
-    if exists:
         #Another speed increase, less to calc
         if is_prime(spiral[0][0]):
             prime_amount += 1
@@ -118,7 +103,7 @@ def calc_prime_corners(spiral):
             prime_amount += 1
         if is_prime(spiral[size][size]): #IT WAS 0 size AAARGH
             prime_amount += 1
-    else:
+    except:
         prime_amount = 0
         for y in range(len(spiral)):
             for x in range(len(spiral[y])):
@@ -131,7 +116,7 @@ def calc_prime_corners(spiral):
     return (prime_amount/((size+1)*2-1)) # 7 gives 8 / 13, so right on
 
 prime_percent = 1
-size = 5
+size = 1
 
 while prime_percent*100 >= 10:
     size += 2

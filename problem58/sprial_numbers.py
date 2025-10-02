9c710fc78874c33006442f5132e2a6def748b774
still-zachary-gibson <zacharygibson487@gmail.com>
2025-10-02 14:13:38 -0400

    range trick seems to work, now for the neato thing

--- problem58/sprial_numbers.py
+++ problem58/sprial_numbers.py
@@ -11,6 +11,16 @@ def is_prime(n): #reused
     
     return True
 
+def print_spiral(spi): #brain hurts
+    for i in spi:
+        temp = []
+        for j in i:
+            if j < 10:
+                temp.append("0" + str(j))
+            else:
+                temp.append(str(j))
+        print(temp)
+
 #I need to optimize, and reuse for some time gain.
 def generate_sprial(size,clockwise=True):
     global the_spiral
@@ -19,6 +29,7 @@ def generate_sprial(size,clockwise=True):
         test = (the_spiral)
     except:
         do_we_have = False
+    cur_dir = 0 #0 right, 1 down, 2 left, 3 up
     if do_we_have: #new code for awesomeness
         #it only works for +2 size increases but i don't care enough currently
         #OH THAT'S SUCH A SPEED INCREASE
@@ -33,40 +44,45 @@ def generate_sprial(size,clockwise=True):
         else:
             number_to_yeah = cool_spiral[size-2][size-2]
             current_postion = [size-2, size-2]
-        spooky_numb = 1
+        #spooky_numb = 1
     else:
         cool_spiral = []
         for i in range(size):
             cool_spiral.append([0]*size)
         number_to_yeah = 1
         current_postion = [math.floor(size/2), math.floor(size/2)] #y, x
-        spooky_numb = math.floor(size/2)
-    cur_dir = 0 #0 right, 1 down, 2 left, 3 up
+        cool_spiral[current_postion[0]][current_postion[1]] = number_to_yeah
+        number_to_yeah += 1
+        current_postion[(cur_dir+1)%2] += 1*(1-(math.floor(cur_dir/2)*2))
+        #spooky_numb = math.floor(size/2)
     allowed_length = 1
     while number_to_yeah <= size**2:
         #cool_spiral[current_postion[0]][current_postion[1]] = number_to_yeah
-        if cur_dir < 2:
-            how_far_go = (size - current_postion[(cur_dir+1)%2])-1
-        elif cur_dir >= 2:
-            how_far_go = (current_postion[(cur_dir+1)%2] - size)+1
         if cur_dir == 0:
-            cool_spiral[current_postion[0]][current_postion[1]:current_postion[1]+how_far_go] = list(range(number_to_yeah, number_to_yeah+how_far_go))
-            current_postion[(cur_dir+1)%2] += how_far_go-1
-            number_to_yeah += how_far_go
+            cool_spiral[current_postion[0]][current_postion[1]:current_postion[1]+allowed_length] = list(range(number_to_yeah, number_to_yeah+allowed_length))
+            current_postion[(cur_dir+1)%2] += allowed_length-1
+            number_to_yeah += allowed_length
         elif cur_dir == 1:
-            for i in range(current_postion[0], current_postion[0]+how_far_go):
+            for i in range(current_postion[0], current_postion[0]+allowed_length):
                 cool_spiral[i][current_postion[1]] = number_to_yeah
                 number_to_yeah += 1
-            current_postion[0] += how_far_go-1
+            current_postion[0] += allowed_length-1
         elif cur_dir == 2:
-            print(cool_spiral[current_postion[0]][current_postion[1]+how_far_go+1:current_postion[1]+1])
-            cool_spiral[current_postion[0]][current_postion[1]+how_far_go+1:current_postion[1]+1] = list(range(number_to_yeah-how_far_go, number_to_yeah,-1))
-            current_postion[0] += how_far_go
-        print(cool_spiral, current_postion)
+            cool_spiral[current_postion[0]][current_postion[1]-allowed_length+1:current_postion[1]+1] = list(range(number_to_yeah+allowed_length-1, number_to_yeah-1,-1))
+            number_to_yeah += allowed_length
+            current_postion[1] -= allowed_length-1
+        elif cur_dir == 3:
+            for i in range(current_postion[0], current_postion[0]-allowed_length, -1):
+                cool_spiral[i][current_postion[1]] = number_to_yeah
+                number_to_yeah += 1
+            current_postion[0] -= allowed_length-1
+        print_spiral(cool_spiral)
+        print(current_postion)
         #look_for = size if cur_dir < 2 else -1 #i didn't notice this was different,,,
         #if current_postion[(cur_dir+1)%2] + spooky_numb*(1-(math.floor(cur_dir/2)*2)) == look_for:
-        #if cur_dir == 1 + clockwise*2:
-        #    spooky_numb -= 1
+        if cur_dir % 2 == 1:
+            if allowed_length+1 < size:
+                allowed_length += 1
         cur_dir = (cur_dir+1*((clockwise*2)-1)) % 4
         current_postion[(cur_dir+1)%2] += 1*(1-(math.floor(cur_dir/2)*2))
         #else:
@@ -120,7 +136,7 @@ size = 5
 while prime_percent*100 >= 10:
     size += 2
 
-    the_spiral = generate_sprial(size, True)
+    the_spiral = generate_sprial(size, False)
 
     #print(the_spiral)
 

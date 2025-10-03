e86f29294ae19f252008af19ddace849c830d55a
still-zachary-gibson <zacharygibson487@gmail.com>
2025-10-03 12:12:27 -0400

    MY GOD IT SLAUGHTERED IT
    I OVERTHOUGHT IT SO HARD

--- problem58/i dont need much of this do i.py
+++ problem58/i dont need much of this do i.py
@@ -1,7 +1,7 @@
 import math
 
 def is_prime(n): #reused
-    if (n < 2 or n % 2 == 0) and n != 2:
+    if (n < 2 or n % 2 == 0):
         return False
     
     for i in range(3, int(math.sqrt(n)) + 1, 2):
@@ -11,22 +11,24 @@ def is_prime(n): #reused
     return True
 
 prime_amount = 0
-side = 0
-size = 3
+side = -1
+size = 1
 cur_numb = 1
 step = 1
 
 prime_percent = 1
 
 while prime_percent >= 0.1:
-    lower = 1 if side == 1 else 0
+    lower = 1 if side == 0 else 0
     if is_prime(cur_numb-lower):
         prime_amount+=1
+    if side == 0 and prime_amount > 0:
+        size += 2
+        prime_percent = prime_amount/((size+1)*2-1)
+        print(size, prime_percent)
     cur_numb += step
-    if side % 2 == 1:
+    if side % 2 == 0 and side >= 0:
         step += 1
     side = (side+1) % 4
-        #(prime_amount/((size+1)*2-1)
-    if side == 1:
-        prime_percent = prime_amount/((size+1)*2-1)
-        size += 2
+
+print(size)

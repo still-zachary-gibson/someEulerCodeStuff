b202faffdacf18f1f3f1fad3ca3f786c27b6859a
still-zachary-gibson <zacharygibson487@gmail.com>
2025-10-07 14:41:24 -0400

    this might be it? still gotta run

--- problem882/im very tired.py
+++ problem882/im very tired.py
@@ -2,18 +2,31 @@
 
 import math
 
+import json
+import os #idk lol
+
 import sys #this just for testin
 
 #this one generates the starting state
 def generate_start(n):
+    if os.path.isfile("saved_start_"+str(n)):
+        with open("saved_start_"+str(n), 'r') as config_file:
+            return json.load(config_file)
     cool = []
     for i in range(1,n+1):
+        print(i)
         for j in range(i): cool.append(num_to_binary(i))
+    with open("saved_start_"+str(n), 'w') as config_file:
+        json.dump(cool, config_file)
     return cool
 
+seen = (0,0)
 #okay lets see here, see how big the number is compared to power of twos...
 #log, probably
 def num_to_binary(n): #input int
+    global seen
+    if seen[0] == n:
+        return seen[1]
     hi_est = (math.floor(math.log2(n)))
     binary = ""
     while hi_est >= 0:
@@ -25,6 +38,7 @@ def num_to_binary(n): #input int
         hi_est -= 1
     #while binary[0] == "0": #Wait, this code is never used.
     #    binary = binary[1:]
+    seen = [n, binary]
     return binary #output str
 
 def binary_to_num(b): #input str
@@ -67,54 +81,12 @@ def any_ones(array): #can zero play any moves?
             break
     return wrong
 
+#okay. So there's always the same number of moves, BUT
+#what 1 and 0 do can determine how many moves 0 must skip
+#1 wants to reduce the choices to all <10 as quick as can
+#while 0 wants to keep it like that as long as possible.
+
 def zero_destroyer(strng): #see how many 0s any move would remove
-    action_taken_str = strng[1:]
-    if len(action_taken_str) < 1: #1 -> 0
-        return (1,0) #it is... a move.
-    while len(action_taken_str) > 0:
-        if action_taken_str[0] == "0":
-            action_taken_str = action_taken_str[1:]
-        else: break
-    else:
-        return (len(strng),0)
-    #1001 -> 001 -> 01 -> 1
-    #okay, goal should be to reduce 0s, but if possible choose ones with more ones?
-    #something about uh,,, making moves that lead to more0s being taken?
-
-    #okay lets think
-    #1000 vs 1001
-    #is one of these better?
-    #okay yeah, 1001 is
-    #cause it gives an extra turn to 1 in the end
-
-    #but does that matter at all to this algorithm?
-    #is there any way that conclusion would affect gameplay?
-
-    #should i also think about 0s gameplan?
-    #okay, 0 would prefer 1000 to 1001?
-    #1000 -> 100 -> 10 -> 1
-    #1001 -> 101 -> 11
-
-    #yeah, 0 would. so 1 should try to go for that one?
-    #there's no way for an extra 1 to pop into existence, isn't there?
-
-    #oh god i think there is, 10 can become 1 or 0, depending on who plays
-    #there is a way to cause extra turns that way, if 1 FORCES 0 to take moves that keep 1s, and 1 tries to not take moves that don't
-    
-    #now how to demonstrate that in code. lets see, 100?
-
-    #right meaning 1 played, v is 0 played
-    #100 > 0
-    #    v 10 > 0
-    #         v 1
-
-    #wait no i  dont think there's extra turns? no, thereisn't. it's just how many turns are used up by 0 skipping that changes...
-    #so 1's goal is to pull the point where 0 has no more moves, ie all numbers < 10, closer...
-    #and 0's goal is to push it farther away.
-
-    #my brain hurts so much
-
-    '''
     max_leading = (0,0)
     for index in range(len(strng)):
         if strng[index] == "0":
@@ -129,7 +101,12 @@ def zero_destroyer(strng): #see how many 0s any move would remove
         if leading > max_leading[0]:
             max_leading = (leading, index)
     return max_leading
-    '''
+    
+#1010, which one 1 remove?
+#X010, or 10X0
+#10 or 100
+
+#10 should be worth more than 1, cause it removes a turn from
 
 #while True:
 #    print(zero_destroyer(input("Enter Binary Num: ")))
@@ -156,22 +133,27 @@ def zero_saver(strng): #find what one saves the most 0s.
 def game_time(start):
     OnesTurn = True
     zeroSkips = 0
+    turn_count = 0
     print("_"*20)
-    print(start, "Turn: None")
+    #print(start, "Turn: None")
     while True:
         if not OnesTurn and not any_ones(start):
             if zeroSkips == 0:
                 print("It begins!")
             OnesTurn = not OnesTurn
+            turn_count+=1
             zeroSkips += 1
             continue
         if sum_with_strs(start) == 0:
             break
         if OnesTurn:
-            best_descruction = (0,0,0)
+            best_descruction = (-1,0,0)
             for index, i in enumerate(start):
                 destr = zero_destroyer(i)
-                if destr[0] >= best_descruction[0]:
+                if destr[0] > best_descruction[0]:
+                    best_descruction = (destr[0], destr[1], index)
+                elif destr[0] >= best_descruction[0] and len(i) > len(start[best_descruction[2]]) and best_descruction[0] != -1:
+                    #print("Did this ever")
                     best_descruction = (destr[0], destr[1], index)
             #how_many,which sub index, which_index
             choice = best_descruction[2]
@@ -185,7 +167,7 @@ def game_time(start):
                         best_descruction = (destr[0], destr[1], index)
                     elif destr[0] >= best_descruction[0] and len(i) > len(start[best_descruction[2]]) and best_descruction[0] != -1:
                         best_descruction = (destr[0], destr[1], index)
-                        print("Better move?")
+                    #    print("Better move?")
             #how_many,which sub index, which_index
             if best_descruction[0] == -1: #apprently this should never be called?
                 print("UHM")
@@ -209,7 +191,9 @@ def game_time(start):
                 start[choice] = start[choice][1:]
         print(start, "Turn: " + str(int(OnesTurn)))
         OnesTurn = not OnesTurn
+        turn_count += 1
     print(f"Zero won, only needing {zeroSkips} skips.")
+    print(turn_count)
     return zeroSkips
         
 if game_time(generate_start(2)) != 2:
@@ -221,6 +205,7 @@ if game_time(generate_start(5)) != 17:
 if game_time(generate_start(10)) != 64:
     print("Still don't got it.")
     sys.exit()
+
 
 game_time(generate_start(10**5))
 #2 = 2

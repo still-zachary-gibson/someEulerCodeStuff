03ba646e0e7b528114ee02253b9eecccfd67363e
still-zachary-gibson <zacharygibson487@gmail.com>
2025-10-08 14:45:40 -0400

    im gonna need to comprhend matrixes a bit more

--- problem101/optimal polynomial.py
+++ problem101/optimal polynomial.py
@@ -16,7 +16,7 @@ def polynomial_ints_outputer(how_many, *coefficents):
     for x in range(how_many[0],how_many[1]+1):
         sum = 0
         for term, n in enumerate(coefficents):
-            sum += n * pow(x, term)
+            sum += n * x**term
         list_of_outputs.append(sum)
 
     return list_of_outputs
@@ -25,4 +25,25 @@ def polynomial_ints_outputer(how_many, *coefficents):
 
 #okay, it works i'm fairly sure,,,
 
-print(polynomial_ints_outputer(10,1,-1,1,-1,1,-1,1,-1,1,-1,1))
+the_canonical_sequence = (polynomial_ints_outputer(10,1,-1,1,-1,1,-1,1,-1,1,-1,1))
+#i can use big fancy words :)
+
+def transpose(old_matrix):
+    new_matrix = []
+    for y in range(len(old_matrix)):
+        new_matrix.append([])
+        for x in range(len(old_matrix[y])):
+            new_matrix[y].append(old_matrix[x][y])
+    return new_matrix
+
+def polynomial_factory(ref_points):
+    x_matrix = []
+    for y in range(len(ref_points)):
+        x_matrix.append([])
+        for x in range(len(ref_points)):
+            x_matrix[y].append((y+1)**x)
+    x_t = transpose(x_matrix)
+    #y_t = transpose([ref_points])
+    #print(y_t)
+    
+polynomial_factory(the_canonical_sequence)
